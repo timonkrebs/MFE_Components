ad4f6dd79d0a68e9e290491ce24a7fcf067d8456
Timon Krebs <timon.krebs@zuehlke.com>
2021-03-30 16:37:26 +0200

    improved environment

--- projects/mfe-shell/src/environments/environment.prod.ts
+++ projects/mfe-shell/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  remoteDraw: "https://flamboyant-bhaskara-04249f.netlify.app/remoteEntry.js"
+  remoteDraw: "https://flamboyant-bhaskara-04249f.netlify.app"
 };
--- projects/mfe-shell/src/environments/environment.ts
+++ projects/mfe-shell/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  remoteDraw: "http://localhost:4202/remoteEntry.js"
+  remoteDraw: "http://localhost:4202"
 };
 
 /*
