c5dad826f37bc0004dd76c93152c1c40b4e19c89
Timon Krebs <timon.krebs@zuehlke.com>
2021-03-31 15:12:16 +0200

    added list component

--- projects/mfe-shell/src/app/shell/components/home/home.component.ts
+++ projects/mfe-shell/src/app/shell/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Select, Store } from "@ngxs/store";
+import { Observable } from "rxjs";
+import { Image } from "projects/mfe-shared/src/lib/app-state/models/Image";
+import { ImageState } from "projects/mfe-shared/src/lib/app-state/state/image.state";
 import { environment } from "../../../../../src/environments/environment";
 
 @Component({
@@ -7,11 +11,12 @@ import { environment } from "../../../../../src/environments/environment";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  @Select(ImageState.getImages) users: Observable<Image[]>;
   remoteDrawUrl;
   loadComponent = false;
 
-  constructor() {
-    this.remoteDrawUrl = `${environment.remoteDraw}/remoteEntry.js`
+  constructor(private store: Store) {
+    this.remoteDrawUrl = `${environment.remoteDraw}/remoteEntry.js`;
   }
 
   ngOnInit(): void {
